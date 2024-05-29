ffa3b0ae1b026c7f0e464e2194887f04b5ded437
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-05-29 21:30:11 +0300

    add icon and meta tags

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -73,14 +73,14 @@ function DesignPreview({ configuration }: { configuration: Configuration }) {
 
             <LoginModal isOpen={isLoginModalOpen} SetIsOpen={setIsLoginModalOpen} />
 
-            <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
-                <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
+            <div className='mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
+                <div className='sm:col-span-4 md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2'>
                     <Phone
-                        className={cn(`bg-${tw}`)}
+                        className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
                         imgScr={configuration.croppedImageUrl!} />
                 </div>
 
-                <div className='mt-6 sm:col-span-9 sm:mt-0 md:row-end-1'>
+                <div className='mt-6 sm:col-span-9 md:row-end-1'>
                     <h3 className='text-3xl font-bold tracking-tight text-gray-900'>Your {modelLabel} Cover</h3>
 
                     <div className='mt-3 flex items-center gap-1.5 text-base'>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import type { Metadata } from "next";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,3 +14,35 @@ export const formatPrice = (price: number, currency: 'EGP' | 'USD') => {
 
   return formatter.format(price);
 };
+
+
+export function constructMetadata({
+  title = 'CobraCover - custom high-quality phone cases',
+  description = 'Create custom high-quality phone cases in seconds',
+  image = '/thumbnail.jpg',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@MohamedAlaa',
+    },
+    icons,
+    metadataBase: new URL("https://cobracover.vercel.app/")
+  }
+}

13df3464c9a61ae40f88935d99eda56b00ee4455
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-05-30 00:08:55 +0300

    fix footer color

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useQuery } from "@tanstack/react-query"
 import { getPaymentStatus } from "./actions"
 import { useSearchParams } from "next/navigation"
@@ -20,11 +19,13 @@ function ThankYou() {
 
     if (data === undefined) {
         return (
-            <div className='w-full mt-24 flex justify-center'>
-                <div className='flex flex-col items-center gap-2'>
-                    <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-                    <h3 className='font-semibold text-xl'>Loading your order...</h3>
-                    <p>This won&apos;t take long.</p>
+            <div className="min-h-[calc(100vh-3.5rem-1px)] bg-white">
+                <div className='w-full mt-24 flex justify-center'>
+                    <div className='flex flex-col items-center gap-2'>
+                        <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
+                        <h3 className='font-semibold text-xl'>Loading your order...</h3>
+                        <p>This won&apos;t take long.</p>
+                    </div>
                 </div>
             </div>
         )
@@ -32,11 +33,13 @@ function ThankYou() {
 
     if (data === false) {
         return (
-            <div className='w-full mt-24 flex justify-center'>
-                <div className='flex flex-col items-center gap-2'>
-                    <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-                    <h3 className='font-semibold text-xl'>Verifying your payment...</h3>
-                    <p>This might take a moment.</p>
+            <div className="min-h-[calc(100vh-3.5rem-1px)] bg-white">
+                <div className='w-full mt-24 flex justify-center'>
+                    <div className='flex flex-col items-center gap-2'>
+                        <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
+                        <h3 className='font-semibold text-xl'>Verifying your payment...</h3>
+                        <p>This might take a moment.</p>
+                    </div>
                 </div>
             </div>
         )
